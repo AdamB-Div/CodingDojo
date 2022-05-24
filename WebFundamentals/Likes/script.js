282ee759c4794483e2e2bbe3459104470b68608a
ABates <divenation14@gmail.com>
2022-05-24 19:53:36 -0400

    final turn in

---
+++ WebFundamentals/Likes/script.js
@@ -0,0 +1,13 @@
+function like() {
+    document.querySelector("#first").innerText++;
+}
+
+function like2() {
+    document.querySelector("#second").innerText++;
+}
+
+var third = document.querySelector("#third");
+
+function like3() {
+    third.innerText++;
+}
