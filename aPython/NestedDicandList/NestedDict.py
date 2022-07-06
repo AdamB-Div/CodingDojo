511ba45ef58765adb12b0df8ed6ce2dbbb3f8b6c
ABates <divenation14@gmail.com>
2022-07-05 23:27:04 -0400

    yeah

--- aPython/NestedDicandList/NestedDict.py
+++ aPython/NestedDicandList/NestedDict.py
@@ -1,40 +1,40 @@
-#==================== 1 ======================== 
-
 # x = [ [5,2,3], [10,8,9] ] 
 # students = [
 #     {'first_name':  'Michael', 'last_name' : 'Jordan'},
 #     {'first_name' : 'John', 'last_name' : 'Rosales'}
 # ]
+
 # sports_directory = {
 #     'basketball' : ['Kobe', 'Jordan', 'James', 'Curry'],
 #     'soccer' : ['Messi', 'Ronaldo', 'Rooney']
 # }
+
 # z = [ {'x': 10, 'y': 20} ]
 
-# # # Update Values
+
+# ============ 1 ===========
+
 # x[1][0] = 15
 # print(x)
 
-# # Change last name
 # students[0]['last_name'] = "Bryant"
 # print(students)
 
-# # # Messi to Andres
 # sports_directory['soccer'][0] = "Andres"
 # print(sports_directory)
 
-# # z 20 to 30
 # z[0]['y'] = 30
 # print(z)
 
-# ==================== 2 =======================
+# ============ 2 ============
 
 # students = [
-#     {'first_name':  'Michael', 'last_name' : 'Jordan'},
-#     {'first_name' : 'John', 'last_name' : 'Rosales'},
-#     {'first_name' : 'Mark', 'last_name' : 'Guillen'},
-#     {'first_name' : 'KB', 'last_name' : 'Tonel'}
-# ]
+#         {'first_name':  'Michael', 'last_name' : 'Jordan'},
+#         {'first_name' : 'John', 'last_name' : 'Rosales'},
+#         {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+#         {'first_name' : 'KB', 'last_name' : 'Tonel'}
+#     ]
+
 # def iterateDictionary(some_list):
 #     for x in range(0, len(some_list)):
 #         temp = ""
@@ -42,9 +42,15 @@
 #             temp += (f"{key} - {val} ")
 #         print(temp)
 
-# iterateDictionary(students)
+# iterateDictionary(students) 
+# should output: (it's okay if each key-value pair ends up on 2 separate lines;
+# bonus to get them to appear exactly as below!)
+# first_name - Michael, last_name - Jordan
+# first_name - John, last_name - Rosales
+# first_name - Mark, last_name - Guillen
+# first_name - KB, last_name - Tonel
 
-# =================== 3 =======================
+# ============ 3 ===============
 
 # students = [
 #     {'first_name':  'Michael', 'last_name' : 'Jordan'},
@@ -61,7 +67,7 @@
 
 # iterateDictionary2("last_name", students)
 
-# ==================== 4 ======================
+# ============ 4 ==============
 
 # dojo = {
 #     'locations': ['San Jose', 'Seattle', 'Dallas', 'Chicago', 'Tulsa', 'DC', 'Burbank'],
