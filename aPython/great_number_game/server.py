9c339d5de35b0d899061f98cf4890476766352cb
ABates <divenation14@gmail.com>
2022-07-09 16:54:53 -0400

    turn in

---
+++ aPython/great_number_game/server.py
@@ -0,0 +1,36 @@
+from flask import Flask, render_template, request, redirect, session
+import random
+
+app = Flask(__name__)
+app.secret_key = 'secret_key'
+
+@app.route("/")
+def index():
+    session.clear()
+    if not session:
+        session['random_int'] = random.randint(1, 100)
+        random_int = session['random_int']
+        return render_template("index.html", random_int = random_int)
+    return render_template("index.html")
+
+@app.route("/wrong")
+def wrong_guess():
+    guess = session['guess']
+    random_int = session['random_int']
+    return render_template("wrong_guess.html", guess = guess, random_int = random_int)
+
+@app.route("/right")
+def right_guess():
+    return render_template("right_guess.html")
+
+@app.route("/guess", methods = ["POST"])
+def guess():
+    session['guess'] = int(request.form['guess'])
+    guess = session['guess']
+    if guess == session['random_int']:
+        return redirect("/right")
+    else:
+        return redirect("/wrong")
+
+if __name__ == "__main__":
+    app.run(debug=True)
