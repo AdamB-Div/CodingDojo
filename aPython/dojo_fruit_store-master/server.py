d4909a51c5fde94ebc38408865679903f0ebdba8
ABates <divenation14@gmail.com>
2022-07-09 12:38:13 -0400

    final

--- aPython/dojo_fruit_store-master/server.py
+++ aPython/dojo_fruit_store-master/server.py
@@ -1,18 +1,27 @@
 from flask import Flask, render_template, request, redirect
-app = Flask(__name__)  
 
-@app.route('/')         
+app = Flask(__name__)
+
+
+@app.route("/")
 def index():
     return render_template("index.html")
 
-@app.route('/checkout', methods=['POST'])         
-def checkout():
-    print(request.form)
-    return render_template("checkout.html")
 
-@app.route('/fruits')         
+@app.route("/checkout", methods=["POST"])
+def checkout():
+    strawberry = int(request.form["strawberry"])
+    raspberry = int(request.form["raspberry"])
+    apple = int(request.form["apple"])
+    total_fruits = strawberry + raspberry + apple
+    print(request.form)
+    return render_template("checkout.html", total_fruits=total_fruits)
+
+
+@app.route("/fruits")
 def fruits():
     return render_template("fruits.html")
 
-if __name__=="__main__":   
-    app.run(debug=True)    
+
+if __name__ == "__main__":
+    app.run(debug=True)
