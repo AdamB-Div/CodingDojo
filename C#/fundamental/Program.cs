835c2c9683fb213327e42a75c8cfaf279e2a683b
ABates <divenation14@gmail.com>
2022-08-01 18:21:52 -0400

    changes

--- C#/fundamental/Program.cs
+++ C#/fundamental/Program.cs
@@ -1,2 +1,55 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 3 Loops, make each function call empty in order to see it work
+
+void numberLoop(int range = 255) {
+    for (int i = 1; i <= range; i++) {
+        Console.WriteLine(i);
+    }
+}
+
+numberLoop(0);
+
+void FizzBuzz(int range = 100) {
+    for (int i = 1; i < range; i++) {
+        bool divisibleByThree = i % 3 == 0;
+        bool divisibleByFive = i % 5 == 0;
+
+        if (divisibleByFive && divisibleByThree) {
+            Console.WriteLine("FizzBuzz");
+        }
+        else if (divisibleByFive){
+            Console.WriteLine("Buzz");
+        }
+        else if (divisibleByThree){
+            Console.WriteLine("Fizz");
+        }
+        else {
+            Console.WriteLine(i);
+        }
+    }
+}
+
+FizzBuzz(0);
+
+void FizzBuzzWhile(int range = 100) {
+    int i = 1;
+    while (i < 100) {
+        bool divisibleByThree = i % 3 == 0;
+        bool divisibleByFive = i % 5 == 0;
+
+        if (divisibleByFive && divisibleByThree) {
+            Console.WriteLine("FizzBuzz");
+        }
+        else if (divisibleByFive){
+            Console.WriteLine("Buzz");
+        }
+        else if (divisibleByThree){
+            Console.WriteLine("Fizz");
+        }
+        else {
+            Console.WriteLine(i);
+        }
+        i++;
+    }
+}
+
+FizzBuzzWhile(0);
