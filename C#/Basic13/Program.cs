40342eb70d01de73af51ba90e8418e14dbdde567
ABates <divenation14@gmail.com>
2022-08-02 17:36:13 -0400

    human turnin

--- C#/Basic13/Program.cs
+++ C#/Basic13/Program.cs
@@ -96,7 +96,131 @@
 //     Console.WriteLine(average);
 // }
 
-// int[] numArr = {2, 10, 3, 85};
+int[] numArr = {2, 10, 3, 85, -2, -4};
 
 // GetAverage(numArr);
+
+
+// 7
+// static int[] OddArray()
+// {
+//     // Write a function that creates, and then returns, an array that contains all the odd numbers between 1 to 255. 
+//     // When the program is done, this array should have the values of [1, 3, 5, 7, ... 255].
+//     int[] result = new int[255];
+//     int idxCount = 0;
+//     for (int i = 1; i < result.Length; i++) {
+//         if (i % 2 == 1){
+//             result[idxCount] = i;
+//             idxCount++;
+//         }
+//     }
+//     return result;
+// }
+
+// int[] array = OddArray();
+// foreach (int num in array){
+//     Console.WriteLine(num);
+// }
+
+
+// 8
+// static int GreaterThanY(int[] numbers, int y)
+// {
+//     // Write a function that takes an integer array, and a integer "y" and returns the number of array values 
+//     // That are greater than the "y" value. 
+//     // For example, if array = [1, 3, 5, 7] and y = 3. Your function should return 2 
+//     // (since there are two values in the array that are greater than 3).
+//     int Count = 0;
+//     for (int i = 0; i < numbers.Length; i++)
+//     {
+//         if (numbers[i] > y)
+//         {
+//             Count++;
+//         }
+//     }
+//     return Count;
+// }
+
+// Console.WriteLine(GreaterThanY(numArr, 3));
+
+
+// 9
+// static void SquareArrayValues(int[] numbers)
+// {
+//     // Write a function that takes an integer array "numbers", and then multiplies each value by itself.
+//     // For example, [1,5,10,-10] should become [1,25,100,100]
+//     for (int i = 0; i < numbers.Length; i++)
+//     {
+//         numbers[i] = (int)numbers[i] * (int)numbers[i];
+//     }
+//     foreach (int num in numbers)
+//     {
+//         Console.WriteLine(num);
+//     }
+// }
+
+// SquareArrayValues(numArr);
+
+
+// 10
+// static void EliminateNegatives(int[] numbers)
+// {
+//     // Given an integer array "numbers", say [1, 5, 10, -2], create a function that replaces any negative number with the value of 0. 
+//     // When the program is done, "numbers" should have no negative values, say [1, 5, 10, 0].
+//     for (int i = 0; i < numbers.Length; i++)
+//     {
+//             if (numbers[i] < 0)
+//             {
+//                 numbers[i] = 0;
+//             }
+//     }
+//     foreach (int num in numbers)
+//     {
+//         Console.WriteLine(num);
+//     }
+// }
+
+// EliminateNegatives(numArr);
+
+
+// 11
+// static void MinMaxAverage(int[] numbers)
+// {
+//     // Given an integer array, say [1, 5, 10, -2], create a function that prints the maximum number in the array, 
+//     // the minimum value in the array, and the average of the values in the array.
+//     int max = numbers[0];
+//     int min = numbers[0];
+//     int sum = 0;
+
+//     for (int i = 0; i < numbers.Length; i++)
+//     {
+//         if (numbers[i] > max)
+//         {
+//             max = numbers[i];
+//         }
+//         if (numbers[i] < min)
+//         {
+//             min = numbers[i];
+//         }
+//         sum = sum += numbers[i];
+//     }
+//     int average = (int)sum / numbers.Length;
+//     Console.WriteLine("The Max Value is " + max);
+//     Console.WriteLine("The Min Value is " + min);
+//     Console.WriteLine("The Average Value is " + average);
+// }
+
+// MinMaxAverage(numArr);
+
+
+// 12
+static void ShiftValues(int[] numbers)
+{
+    // Given an integer array, say [1, 5, 10, 7, -2], 
+    // Write a function that shifts each number by one to the front and adds '0' to the end. 
+    // For example, when the program is done, if the array [1, 5, 10, 7, -2] is passed to the function, 
+    // it should become [5, 10, 7, -2, 0].
+    List<int> temp = numbers.ToList();
+    temp.RemoveAt(0);
+}
 
---
+++ C#/Human/Program.cs
@@ -0,0 +1,9 @@
+﻿using Humans;
+
+Human alex = new Human("Alex Miller");
+Human martha = new Human("Martha Smith", 20, 20, 20, 500);
+
+Console.WriteLine(alex.Name);
+Console.WriteLine(martha.AllAttributes());
+
+alex.Attack(martha);
