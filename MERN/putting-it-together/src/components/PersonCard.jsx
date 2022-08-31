1e630717756f4279810c579ea8c748bec333d295
ABates <divenation14@gmail.com>
2022-08-31 16:17:46 -0400

    revise

--- MERN/putting-it-together/src/components/PersonCard.jsx
+++ MERN/putting-it-together/src/components/PersonCard.jsx
@@ -3,7 +3,15 @@ import React from "react";
 class PersonCard extends React.Component {
     constructor(props) {
         super(props);
+
+        this.state = {
+            age: this.props.age,
+        };
     }
+
+    handleClick = () => {
+        this.setState({ age: this.state.age + 1 });
+    };
 
     render = () => {
         const { firstName, lastName, age, hairColor } = this.props;
@@ -12,8 +20,11 @@ class PersonCard extends React.Component {
                 <h3>
                     {lastName}, {firstName}
                 </h3>
-                <p>Age: {age}</p>
+                <p>Age: {this.state.age}</p>
                 <p>Hair Color: {hairColor}</p>
+                <button onClick={this.handleClick}>
+                    Birthday Button for {firstName} {lastName}
+                </button>
                 <br />
             </>
         );
