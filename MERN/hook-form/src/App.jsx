d6f93bba6431c9e37c7362c8770b2cc9d2cd5aaa
ABates <divenation14@gmail.com>
2022-09-12 17:18:14 -0400

    turnin

--- MERN/hook-form/src/App.jsx
+++ MERN/hook-form/src/App.jsx
@@ -3,9 +3,6 @@ import HookForm from "./components/HookForm";
 const App = () => {
     return (
         <div className="App">
-            <h1 className="text-3xl font-bold underline text-teal-500">
-                Hello world!
-            </h1>
             <HookForm />
         </div>
     );
--- MERN/hook-form/src/components/HookForm.jsx
+++ MERN/hook-form/src/components/HookForm.jsx
@@ -1,11 +1,74 @@
-import React from "react";
+import React, { useState } from "react";
 
-class HookForm extends React.Component {
-    constructor(props) {
-        super(props);
-    }
+const HookForm = props => {
+    const [firstName, setFirstName] = useState("");
+    const [lastName, setLastName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [confirmPassword, setConfirmPassword] = useState("");
 
-    render = () => {
-        return ()
-    }
+    return (
+        <div>
+            <form className="flex flex-col">
+                <label>First Name:</label>
+                <input
+                    onChange={e => {
+                        setFirstName(e.target.value);
+                    }}
+                    type="text"
+                    value={firstName}
+                    className="border"
+                />
+
+                <label>Last Name:</label>
+                <input
+                    onChange={e => {
+                        setLastName(e.target.value);
+                    }}
+                    type="text"
+                    value={lastName}
+                    className="border"
+                />
+
+                <label>Email:</label>
+                <input
+                    onChange={e => {
+                        setEmail(e.target.value);
+                    }}
+                    type="email"
+                    value={email}
+                    className="border"
+                />
+
+                <label>Password:</label>
+                <input
+                    onChange={e => {
+                        setPassword(e.target.value);
+                    }}
+                    type="password"
+                    value={password}
+                    className="border"
+                />
+
+                <label>Confirm Password:</label>
+                <input
+                    onChange={e => {
+                        setConfirmPassword(e.target.value);
+                    }}
+                    type="password"
+                    value={confirmPassword}
+                    className="border"
+                />
+            </form>
+
+            <h3>Your Form Submission</h3>
+            <p>{firstName}</p>
+            <p>{lastName}</p>
+            <p>{email}</p>
+            <p>{password}</p>
+            <p>{confirmPassword}</p>
+        </div>
+    );
 };
+
+export default HookForm;
