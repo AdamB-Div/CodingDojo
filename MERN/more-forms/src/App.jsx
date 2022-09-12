ce4f8e1163d0a03c8de3eea3cebec041b0f8779d
ABates <divenation14@gmail.com>
2022-09-12 17:44:56 -0400

    initial

---
+++ MERN/more-forms/src/App.jsx
@@ -0,0 +1,11 @@
+import HookForm from "./components/HookForm";
+
+const App = () => {
+    return (
+        <div className="App">
+            <HookForm />
+        </div>
+    );
+};
+
+export default App;
