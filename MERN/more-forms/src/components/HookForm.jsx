4ee52f662d7c1e3c94a39daebd151a1bc4729de4
ABates <divenation14@gmail.com>
2022-09-13 14:20:10 -0400

    turnin

--- MERN/more-forms/src/components/HookForm.jsx
+++ MERN/more-forms/src/components/HookForm.jsx
@@ -2,59 +2,104 @@ import React, { useState } from "react";
 
 const HookForm = props => {
     const [firstName, setFirstName] = useState("");
+    const [firstNameError, setFirstNameError] = useState("");
     const [lastName, setLastName] = useState("");
+    const [lastNameError, setLastNameError] = useState("");
     const [email, setEmail] = useState("");
+    const [emailError, setEmailError] = useState("");
     const [password, setPassword] = useState("");
+    const [passwordError, setPasswordError] = useState("");
     const [confirmPassword, setConfirmPassword] = useState("");
+    const [confirmPasswordError, setConfirmPasswordError] = useState("");
+
+    const handleFirstName = e => {
+        setFirstName(e.target.value);
+        if (e.target.value.length < 2) {
+            setFirstNameError("First Name must be at least 2 characters");
+        } else {
+            setFirstNameError("");
+        }
+    };
+
+    const handleLastName = e => {
+        setLastName(e.target.value);
+        if (e.target.value.length < 2) {
+            setLastNameError("Last Name must be at least 2 characters");
+        } else {
+            setLastNameError("");
+        }
+    };
+
+    const handleEmail = e => {
+        setEmail(e.target.value);
+        if (e.target.value.length < 5) {
+            setEmailError("Email must be at least 5 characters");
+        } else {
+            setEmailError("");
+        }
+    };
+
+    const handlePassword = e => {
+        setPassword(e.target.value);
+        if (e.target.value.length < 5) {
+            setPasswordError("Password must be at least 8 characters");
+        } else {
+            setPasswordError("");
+        }
+    };
+
+    const handleConfirmPassword = e => {
+        setConfirmPassword(e.target.value);
+        if (e.target.value !== { password }) {
+            setConfirmPasswordError("Passwords must match");
+        } else {
+            setPasswordError("");
+        }
+    };
 
     return (
         <div>
             <form className="flex flex-col">
                 <label>First Name:</label>
                 <input
-                    onChange={e => {
-                        setFirstName(e.target.value);
-                    }}
+                    onChange={handleFirstName}
                     type="text"
                     value={firstName}
                     className="border"
                 />
+                {firstNameError ? <p>{firstNameError}</p> : ""}
 
                 <label>Last Name:</label>
                 <input
-                    onChange={e => {
-                        setLastName(e.target.value);
-                    }}
+                    onChange={handleLastName}
                     type="text"
                     value={lastName}
                     className="border"
                 />
+                {lastNameError ? <p>{lastNameError}</p> : ""}
 
                 <label>Email:</label>
                 <input
-                    onChange={e => {
-                        setEmail(e.target.value);
-                    }}
+                    onChange={handleEmail}
                     type="email"
                     value={email}
                     className="border"
                 />
+                {emailError ? <p>{emailError}</p> : ""}
 
                 <label>Password:</label>
                 <input
-                    onChange={e => {
-                        setPassword(e.target.value);
-                    }}
+                    onChange={handlePassword}
                     type="password"
                     value={password}
                     className="border"
                 />
+                {passwordError ? <p>{passwordError}</p> : ""}
 
+                {confirmPasswordError ? <p>{confirmPasswordError}</p> : ""}
                 <label>Confirm Password:</label>
                 <input
-                    onChange={e => {
-                        setConfirmPassword(e.target.value);
-                    }}
+                    onChange={handleConfirmPassword}
                     type="password"
                     value={confirmPassword}
                     className="border"
