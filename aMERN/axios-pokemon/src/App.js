765ea19fcb7c36d81d798eb2f0c9db63cbf625be
ABates <divenation14@gmail.com>
2022-11-01 18:51:41 -0400

    turnin

--- aMERN/axios-pokemon/src/App.js
+++ aMERN/axios-pokemon/src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [pokemon807, setPokemon807] = useState([]);
+
+    const setPokemon = () => {
+        axios
+            .get("https://pokeapi.co/api/v2/pokemon?limit=807")
+            .then(res => {
+                setPokemon807(res.data.results);
+            })
+            .catch(err => {
+                console.log(err);
+            });
+    };
+
+    return (
+        <div className="flex flex-col m-auto items-center gap-4 p-4">
+            <button
+                className="border-2 border-black rounded bg-slate-200 px-2 w-2/5"
+                onClick={setPokemon}
+            >
+                Fetch "All" 807 Pokemon
+            </button>
+            <ul className="list-disc">
+                {pokemon807.map((p, i) => {
+                    return <li key={i}>{p.name}</li>;
+                })}
+            </ul>
+        </div>
+    );
 }
 
 export default App;
